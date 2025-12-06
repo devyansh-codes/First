d3913b0b22f21abc90c448faef25acfdd96525ee
Devyansh <mahatha.devyansh@gmail.com>
2025-12-06 23:00:34 +0530

    Add program to delete an element from an array

---
+++ delete from position.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    int arr[100], n, pos, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d integers:\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    printf("Enter position to delete (0 to %d): ", n-1);
+    scanf("%d", &pos);
+    if(pos < 0 || pos >= n) {
+        printf("Invalid position!");
+    }
+    for(i = pos; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+    n--;  
+    printf("Array after deletion:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
